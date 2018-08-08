c67c7502a54619baef4f8eba5ffdabf894448974
haowei4032 <boss@haowei.me>
2018-08-08 11:10:51 -0400

    Initial commit

--- logger.c
+++ logger.c
@@ -221,3 +221,11 @@ ZEND_TSRMLS_CACHE_DEFINE()
 ZEND_GET_MODULE(logger)
 #endif
 
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * End:
+ * vim600: noet sw=4 ts=4 fdm=marker
+ * vim<600: noet sw=4 ts=4
+ */
