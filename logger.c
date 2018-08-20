92984881e9e511c66c665a418feedda032e1fac4
haowei4032 <boss@haowei.me>
2018-08-19 21:40:17 -0400

    Initial commit

--- logger.c
+++ logger.c
@@ -23,18 +23,71 @@
 static void php_logger_init_globals(zend_logger_globals *logger_globals TSRMLS_DC)
 {}
 
-static void logger_clean()
+static void logger_environment_init()
 {
-	zval *val;
-	php_stream *stream;
-	stream = php_stream_open_wrapper("/var/log/app-2018-08-11.log", "ab", IGNORE_PATH|IGNORE_URL_WIN, NULL);
-	if (stream == NULL) return;
-	ZEND_HASH_FOREACH_VAL(Z_ARRVAL_P(&LOGGER_G(async_stack)), val) {
-		php_printf("%s\n", Z_STRVAL_P(val));
-	} ZEND_HASH_FOREACH_END();
-	php_stream_close(stream);
+	int i, n, len;
+	char format[1];
+	char date_format[3] = "Ymd";
 
-	zval_ptr_dtor(&LOGGER_G(async_stack));
+	zval rv, list, rotate, filepath;
+	zend_string *str, *filename;
+
+	ZVAL_STRING(&filepath, INI_STR("logger.path"));
+	php_stat(Z_STRVAL(filepath), Z_STRLEN(filepath), FS_IS_DIR, &rv);
+	if (Z_TYPE_P(&rv) == IS_FALSE) {
+		if (!php_stream_mkdir(Z_STRVAL(filepath), 0755, PHP_STREAM_MKDIR_RECURSIVE, NULL)) {
+			php_error_docref(NULL, E_ERROR, "EastWood Log directory creation failed %s", Z_STRVAL(filepath));
+			return ;
+		}
+	}
+
+	php_stat(Z_STRVAL(filepath), Z_STRLEN(filepath), FS_IS_W, &rv);
+	if (Z_TYPE_P(&rv) == IS_FALSE) {
+		php_error_docref(NULL, E_ERROR, "EastWood Log directory does not have write access %s", Z_STRVAL(filepath));
+		return ;
+	}
+
+	array_init(&list);
+	add_next_index_string(&list, LOGGER_FILENAME_PREFIX);
+
+	ZVAL_STRING(&rotate, INI_STR("logger.rotate"));
+
+	for(i = 0; i <= logger_rotate_intval(Z_STRVAL(rotate)); i++) {
+		format[0] = date_format[i];
+		str = php_format_date(format, 1, time(NULL), 1);
+		add_next_index_string(&list, ZSTR_VAL(str));
+		zend_string_release(str);
+	}
+
+	str = zend_string_init("-", 1, 0);
+	php_implode(str, &list, &rv);
+	
+	zval_ptr_dtor(&rv);
+	zend_string_release(str);
+
+	str = zend_string_init(Z_STRVAL(filepath), Z_STRLEN(filepath), 0);
+	filename = zend_string_concat_ex(str, 3, "/", Z_STRVAL(rv), LOGGER_FILENAME_AFTERFIX);
+	zend_string_release(str);
+
+	php_stat(ZSTR_VAL(filename), ZSTR_LEN(filename), FS_IS_FILE, &rv);
+	if (Z_TYPE_P(&rv) == IS_TRUE) {
+		//zval_ptr_dtor(&rv);
+		php_stat(ZSTR_VAL(filename), ZSTR_LEN(filename), FS_IS_W, &rv);
+		if (Z_TYPE_P(&rv) == IS_FALSE) {
+			php_error_docref(NULL, E_ERROR, "EastWood Log file has no write permission %s", ZSTR_VAL(filename));
+			return ;
+		}
+	}
+
+	//ZVAL_STR(&LOGGER_G(logging_filename), filename);
+	LOGGER_G(logging_filename) = ZSTR_VAL(filename);
+	php_printf("filename.first: %s\n", LOGGER_G(logging_filename));
+
+	zval_ptr_dtor(&rv);
+	zval_ptr_dtor(&list);
+	zval_ptr_dtor(&rotate);
+	zval_ptr_dtor(&filepath);
+	zend_string_release(filename);
 }
 
 static void logger_init()
@@ -44,93 +97,135 @@ static void logger_init()
 	zend_update_static_property(logger_ce, ZEND_STRL(LOGGER_CLASS_PROPERTY_NAME), &init TSRMLS_CC);
 	zval_ptr_dtor(&init);
 
-	array_init_size(&LOGGER_G(async_stack), 0);
+	array_init_size(&LOGGER_G(logging_stack), 0);
+
+}
+
+static void logger_cleaner()
+{
+	php_printf("filename.last: %s\n", LOGGER_G(logging_filename));
+
+	zval *val;
+	//php_stream *stream = php_stream_open_wrapper(Z_STRVAL(LOGGER_G(logging_filename)), "ab", IGNORE_PATH|IGNORE_URL_WIN, NULL);
+	//if (stream == NULL) return;
+	//php_printf("filename: %s\n", Z_STRVAL_P(&LOGGER_G(logging_filename)));
+	ZEND_HASH_FOREACH_VAL(Z_ARRVAL_P(&LOGGER_G(logging_stack)), val) {
+		php_printf("yield: %s\n", Z_STRVAL_P(val));
+		//php_stream_write(stream, Z_STRVAL_P(val), Z_STRLEN_P(val));
+		//zval_ptr_dtor(val);
+	} ZEND_HASH_FOREACH_END();
+	//php_stream_close(stream);
+
+	//zval_ptr_dtor(&LOGGER_G(logging_filename));
+	zval_ptr_dtor(&LOGGER_G(logging_stack));
+}
+
+static void logger_format(zval *result, char *search, size_t search_len, char *replace, size_t replace_len)
+{
+	zend_string *rv = php_str_to_str(
+		Z_STRVAL_P(result),
+		Z_STRLEN_P(result),
+		search,
+		search_len,
+		replace,
+		replace_len
+	);
+	zval_ptr_dtor(result);
+	ZVAL_STR(result, rv);
+	zend_string_free(rv);
 }
 
 static int logger_factory(INTERNAL_FUNCTION_PARAMETERS, int level)
 {
-	int i, n, len;
-	char format[1];
-	char date_format[3] = "Ymd";
+	zval rv, level_list;
+	zend_string *tmp, *tag, *message, *output;
 
-	php_stream *stream;
-	zval rv, pool, filepath, rotate;
-	zend_string *tag, *message, *str, *filename;
-
-	if (ZEND_NUM_ARGS() < 2) {
-		php_error_docref(NULL, E_ERROR, "expects exactly 2 parameters, %d given", ZEND_NUM_ARGS());
-		return FAILURE;
-	}
+	array_init(&level_list);
+	add_next_index_string(&level_list, LOGGER_LEVEL_STR_INFO);
+	add_next_index_string(&level_list, LOGGER_LEVEL_STR_WARNING);
+	add_next_index_string(&level_list, LOGGER_LEVEL_STR_ERROR);
+	add_next_index_string(&level_list, LOGGER_LEVEL_STR_DEBUG);
+	add_next_index_string(&level_list, LOGGER_LEVEL_STR_VERBOSE);
 
 	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "SS", &tag, &message) == FAILURE) {
 		return FAILURE;
 	}
 
-	ZVAL_STRING(&filepath, INI_STR("logger.path"));
-	php_stat(Z_STRVAL(filepath), Z_STRLEN(filepath), FS_IS_DIR, &rv);
-	if (Z_TYPE_P(&rv) == IS_FALSE) {
-		if (!php_stream_mkdir(Z_STRVAL(filepath), 0755, PHP_STREAM_MKDIR_RECURSIVE, NULL)) {
-			php_error_docref(NULL, E_ERROR, "EastWood Log directory creation failed %s", Z_STRVAL(filepath));
-			return FAILURE;
-		}
-	}
-
-	php_stat(Z_STRVAL(filepath), Z_STRLEN(filepath), FS_IS_W, &rv);
-	if (Z_TYPE_P(&rv) == IS_FALSE) {
-		php_error_docref(NULL, E_ERROR, "EastWood Log directory does not have write access %s", Z_STRVAL(filepath));
-		return FAILURE;
-	}
-
-	array_init(&pool);
-	add_next_index_string(&pool, LOGGER_FILENAME_PREFIX);
-
-	ZVAL_STRING(&rotate, INI_STR("logger.rotate"));
-
-	for(i = 0; i <= logger_rotate_intval(Z_STRVAL(rotate)); i++) {
-		format[0] = date_format[i];
-		str = php_format_date(format, 1, time(NULL), 1);
-		add_next_index_string(&pool, ZSTR_VAL(str));
-		zend_string_release(str);
-	}
-
-	str = zend_string_init("-", 1, 0);
-	php_implode(str, &pool, &rv);
-	zend_string_release(str);
-
-	str = zend_string_init(Z_STRVAL(filepath), Z_STRLEN(filepath), 0);
-	filename = zend_string_concat_ex(str, 3, "/", Z_STRVAL(rv), LOGGER_FILENAME_AFTERFIX);
-
 	zval_ptr_dtor(&rv);
-	php_stat(ZSTR_VAL(filename), ZSTR_LEN(filename), FS_IS_FILE, &rv);
-	if (Z_TYPE_P(&rv) == IS_TRUE) {
-		//zval_ptr_dtor(&rv);
-		php_stat(ZSTR_VAL(filename), ZSTR_LEN(filename), FS_IS_W, &rv);
-		if (Z_TYPE_P(&rv) == IS_FALSE) {
-			php_error_docref(NULL, E_ERROR, "EastWood Log file has no write permission %s", ZSTR_VAL(filename));
-			return FAILURE;
-		}
+
+	logger_environment_init();
+
+	php_printf("filename.second: %s\n", LOGGER_G(logging_filename));
+
+	if (Z_TYPE(PG(http_globals)[TRACK_VARS_SERVER]) == IS_ARRAY) {
+		php_printf("_SERVER is exists\n");
 	}
 
-	add_next_index_string(&LOGGER_G(async_stack), ZSTR_VAL(message));
 
-	/*stream = php_stream_open_wrapper(ZSTR_VAL(filename), "ab", IGNORE_PATH|IGNORE_URL_WIN, NULL);
-	if (stream == NULL) return FAILURE;
 
-	len = ZSTR_LEN(message);
-	message = zend_string_concat(message, "\n");
-	php_stream_write(stream, ZSTR_VAL(message), ZSTR_LEN(message));
-	php_stream_close(stream);*/
+	ZVAL_STRING(&rv, INI_STR("logger.format"));
+	//php_printf("format: %s\n", Z_STRVAL(rv));
 
-	zend_string_release(str);
-	zend_string_release(message);
-	zend_string_release(filename);
+
+
+	string_trim(INI_STR("logger.application"), &tmp);
+	output = zend_strpprintf(0, "%s", ZSTR_LEN(tmp) == 0 ? "-" : ZSTR_VAL(tmp));
+	logger_format(&rv, "{app}", sizeof("{app}") - 1, ZSTR_VAL(output), ZSTR_LEN(output));
+	logger_format(&rv, "{application}", sizeof("{application}") - 1, ZSTR_VAL(output), ZSTR_LEN(output));
+	zend_string_release(tmp);
+	zend_string_release(output);
+
+	output = php_format_date("r", 1, time(NULL), 1);
+	logger_format(&rv, "{date_rfc}", sizeof("{date_rfc}") - 1, ZSTR_VAL(output), ZSTR_LEN(output));
+	zend_string_release(output);
+
+	php_printf("filename.third: %s\n", LOGGER_G(logging_filename));
+
+	output = get_host_name();
+	logger_format(&rv, "{hostname}", sizeof("{hostname}") - 1, ZSTR_VAL(output), ZSTR_LEN(output));
+	zend_string_release(output);
+
+	output = zend_strpprintf(0, "%s", get_current_user());
+	logger_format(&rv, "{current_user}", sizeof("{current_user}") - 1, ZSTR_VAL(output), ZSTR_LEN(output));
+	zend_string_release(output);
+
+	output = zend_strpprintf(0, "%.3f", microtime());
+	logger_format(&rv, "{timestamp}", sizeof("{timestamp}") - 1, ZSTR_VAL(output), ZSTR_LEN(output));
+	zend_string_release(output);
+
+	zval *val = zend_hash_index_find(Z_ARRVAL(level_list), level);
+	output = zend_strpprintf(0, "%s", Z_STRVAL_P(val));
+	logger_format(&rv, "{level}", sizeof("{level}") - 1, ZSTR_VAL(output), ZSTR_LEN(output));
+	zval_ptr_dtor(val);
+	zend_string_release(output);
+
+	output = zend_strpprintf(0, "%d", getgid());
+	logger_format(&rv, "{gid}", sizeof("{gid}") - 1, ZSTR_VAL(output), ZSTR_LEN(output));
+	zend_string_release(output);
+
+	output = zend_strpprintf(0, "%d", getpid());
+	logger_format(&rv, "{pid}", sizeof("{pid}") - 1, ZSTR_VAL(output), ZSTR_LEN(output));
+	zend_string_release(output);
+
+	logger_format(&rv, "{tag}", sizeof("{tag}") - 1, ZSTR_VAL(tag), ZSTR_LEN(tag));
+	logger_format(&rv, "{message}", sizeof("{message}") - 1, ZSTR_VAL(message), ZSTR_LEN(message));
+
 	
-	zval_ptr_dtor(&rv);
-	zval_ptr_dtor(&pool);
-	zval_ptr_dtor(&rotate);
-	zval_ptr_dtor(&filepath);
 
-	return len;
+
+	add_next_index_string(&LOGGER_G(logging_stack), Z_STRVAL(rv));
+	zval_ptr_dtor(&rv);
+	zval_ptr_dtor(&level_list);
+
+	php_printf("filename.fouth: %s\n", LOGGER_G(logging_filename));
+
+	return SUCCESS;
+}
+
+
+PHP_FUNCTION(logger_version)
+{
+	RETURN_STRING(PHP_LOGGER_VERSION);
 }
 
 PHP_METHOD(logger, info)
@@ -186,7 +281,7 @@ PHP_RINIT_FUNCTION(logger)
 
 PHP_RSHUTDOWN_FUNCTION(logger)
 {
-	logger_clean();
+	logger_cleaner();
 	return SUCCESS;
 }
 
--- php_logger.h
+++ php_logger.h
@@ -22,10 +22,13 @@
 #include "config.h"
 #endif
 
+#include <pwd.h>
 #include "php.h"
 #include "php_ini.h"
+#include "SAPI.h"
 #include <ext/date/php_date.h>
 #include <ext/standard/info.h>
+#include <ext/standard/php_string.h>
 #include <ext/standard/php_filestat.h>
 
 #ifndef PHP_LOGGER_H
@@ -34,7 +37,7 @@
 extern zend_module_entry logger_module_entry;
 #define phpext_logger_ptr &logger_module_entry
 
-#define PHP_LOGGER_VERSION "0.1.0"
+#define PHP_LOGGER_VERSION "1.0.0"
 
 #ifdef PHP_WIN32
 #	define PHP_LOGGER_API __declspec(dllexport)
@@ -71,23 +74,33 @@ ZEND_TSRMLS_CACHE_EXTERN()
 #define LOGGER_FILENAME_PREFIX          "app"
 #define LOGGER_FILENAME_AFTERFIX        ".log"
 
+#define LOGGER_LEVEL_STR_INFO           "info"
+#define LOGGER_LEVEL_STR_WARNING        "warning"
+#define LOGGER_LEVEL_STR_ERROR          "error"
+#define LOGGER_LEVEL_STR_DEBUG          "debug"
+#define LOGGER_LEVEL_STR_VERBOSE        "verbose"
+
 #define LOGGER_LEVEL_INFO               0
 #define LOGGER_LEVEL_WARNING            1
 #define LOGGER_LEVEL_ERROR              2
 #define LOGGER_LEVEL_DEBUG              3
 #define LOGGER_LEVEL_VERBOSE            4
 
+
 #define LOGGER_ROTATE_YEAR              0
 #define LOGGER_ROTATE_MONTH             1
-#define LOGGER_ROTATE_DAILY			        2
+#define LOGGER_ROTATE_DAILY			    2
 
 static zend_class_entry *logger_ce, ce;
 
 ZEND_BEGIN_MODULE_GLOBALS(logger)
-  zval async_stack;
+  zval logging_stack;
+  zval logging_filename;
 ZEND_END_MODULE_GLOBALS(logger)
 
 ZEND_DECLARE_MODULE_GLOBALS(logger)
+
+PHP_FUNCTION(logger_version);
 
 PHP_METHOD(logger, info);
 PHP_METHOD(logger, warning);
@@ -98,7 +111,7 @@ PHP_METHOD(logger, verbose);
 PHP_INI_BEGIN()
   PHP_INI_ENTRY("logger.path", "/var/log", PHP_INI_ALL, NULL)
   PHP_INI_ENTRY("logger.rotate", "", PHP_INI_ALL, NULL)
-  PHP_INI_ENTRY("logger.format", "", PHP_INI_ALL, NULL)
+  PHP_INI_ENTRY("logger.format", "{date_rfc}||{timestamp}||{level}||{tag}||{message}", PHP_INI_ALL, NULL)
   PHP_INI_ENTRY("logger.application", "", PHP_INI_ALL, NULL)
 PHP_INI_END()
 
@@ -112,6 +125,7 @@ const zend_function_entry logger_methods[] = {
 };
 
 const zend_function_entry logger_functions[] = {
+    PHP_FE(logger_version, NULL)
     PHP_FE_END
 };
 
@@ -151,6 +165,83 @@ static zval* call_user_func(const char *func_name, int num, zval param[])
     call_user_function(EG(function_table), NULL, &function, &rv, num, param);
     rv_ptr = &rv;
     return rv_ptr;
+}
+
+static double microtime()
+{
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    return tv.tv_sec + (double)tv.tv_usec / 1000000;
+}
+
+static char *get_current_user()
+{
+    struct passwd *pwd;
+    pwd = getpwuid(getuid());
+    return pwd->pw_name;
+}
+
+static void string_trim(const char *subject, zend_string **result)
+{
+    zend_string *tmp = zend_strpprintf(0, "%s", subject);
+    *result = php_trim(tmp, NULL, 0, 3);
+    zend_string_release(tmp);
+}
+
+static zend_string* get_host_name()
+{
+    char buf[255];
+    zend_string *result;
+    gethostname(buf, sizeof(buf));
+    string_trim(buf, &result);
+    return result;
+}
+
+
+static void get_strval(const char *key, zval *val)
+{
+    //zend_string *zkey;
+    //zkey = zend_strpprintf(0, "_SERVER");
+    //val = zend_hash_find(&EG(symbol_table), zkey);
+    //zend_string_release(zkey);
+
+    /*zend_long num_key;
+    zend_string *str_key;
+    zval *v;
+    ZEND_HASH_FOREACH_KEY_VAL(zend_rebuild_symbol_table(), num_key, str_key, v) {
+        if (Z_TYPE_P(v) == IS_STRING) {
+            php_printf("(string) %s=%s\n", ZSTR_VAL(str_key), Z_STRVAL_P(v));
+        } else if (Z_TYPE_P(v) == IS_DOUBLE) {
+            php_printf("(double) %s=%d\n", ZSTR_VAL(str_key), Z_DVAL_P(v));
+        } else if (Z_TYPE_P(v) == IS_ARRAY) {
+            php_printf("(array) %s=[array]\n", ZSTR_VAL(str_key));
+        }
+    } ZEND_HASH_FOREACH_END();*/
+
+    /*if (zend_is_auto_global_str("_SERVER", sizeof("_SERVER") - 1) == SUCCESS) {
+        php_printf("_SERVER is exists\n");
+    }else{
+        php_printf("_SERVER not exists\n");
+    }*/
+
+    php_printf("uri: %s\n", SG(request_info).request_uri);
+    php_printf("verb: %s\n", SG(request_info).request_method);
+
+
+    //if (val == NULL) php_printf("_SERVER: nil\n");
+
+
+    //zkey = zend_strpprintf(0, "%s", key);
+    //php_printf("b: %d\n", zend_hash_exists(ht, zkey));
+
+    /*zval *v;
+    ZEND_HASH_FOREACH_VAL(ht, v) {
+        if (Z_TYPE_P(v) == IS_STRING) {
+            php_printf("strval: %s\n", Z_STRVAL_P(v));
+        }else if(Z_TYPE_P(v) == IS_DOUBLE) {
+            php_printf("dval: %d\n", Z_DVAL_P(v));
+        }
+    } ZEND_HASH_FOREACH_END();*/
 }
 
 /*
