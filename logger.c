670f675d90dcd4bc5f4191637f016c3d8e6cf00d
haowei4032 <boss@haowei.me>
2018-08-23 06:29:47 -0400

    Initial commit v1.0.0

--- logger.c
+++ logger.c
@@ -23,289 +23,315 @@
 static void php_logger_init_globals(zend_logger_globals *logger_globals TSRMLS_DC)
 {}
 
+static void logger_begin()
+{
+    zend_string *server = zend_string_init(ZEND_STRL("_SERVER"), 0);
+    zend_is_auto_global(server);
+    zend_string_free(server);
+
+    zval init;
+    array_init_size(&init, 0);
+    zend_update_static_property(logger_ce, ZEND_STRL("settings"), &init TSRMLS_CC);
+    zval_ptr_dtor(&init);
+
+    array_init_size(&LOGGER_G(logging_stack), 0);
+}
+
+static void logger_end()
+{
+    zval *val;
+    ZEND_HASH_FOREACH_VAL(Z_ARRVAL_P(&LOGGER_G(logging_stack)), val) {
+        php_printf("yield: %s\n", Z_STRVAL_P(val));
+    } ZEND_HASH_FOREACH_END();
+
+    php_printf("filename.last: %s\n", Z_STRVAL(LOGGER_G(logging_filename)));
+
+    zval_ptr_dtor(val);
+    zval_ptr_dtor(&LOGGER_G(logging_filename));
+    zval_ptr_dtor(&LOGGER_G(logging_stack));
+}
+
+static void logger_format_line(zval *line, char *search, char *replace)
+{
+    /*zend_string *value = php_str_to_str(
+        Z_STRVAL_P(line),
+        Z_STRLEN_P(line),
+        search,
+        strlen(search),
+        replace,
+        replace_len
+    );
+    zval_ptr_dtor(line);
+    ZVAL_STR(line, value);
+    zend_string_free(value);*/
+
+    char *value = string_replace(Z_STRVAL_P(line), search, replace);
+    zval_ptr_dtor(line);
+    ZVAL_STRING(line, value);
+    free(value);
+}
+
 static void logger_environment_init()
 {
-	int i, n, len;
-	char format[1];
-	char date_format[3] = "Ymd";
+    int i, n, len;
+    char format[1];
+    char date_format[3] = "Ymd";
 
-	zval rv, list, rotate, filepath;
-	zend_string *str, *filename;
+    zval rv, list, rotate, filepath, filename;
+    zend_string *str;
 
-	ZVAL_STRING(&filepath, INI_STR("logger.path"));
-	php_stat(Z_STRVAL(filepath), Z_STRLEN(filepath), FS_IS_DIR, &rv);
-	if (Z_TYPE_P(&rv) == IS_FALSE) {
-		if (!php_stream_mkdir(Z_STRVAL(filepath), 0755, PHP_STREAM_MKDIR_RECURSIVE, NULL)) {
-			php_error_docref(NULL, E_ERROR, "EastWood Log directory creation failed %s", Z_STRVAL(filepath));
-			return ;
-		}
-	}
+    ZVAL_STRING(&filepath, INI_STR("logger.path"));
+    php_stat(Z_STRVAL(filepath), Z_STRLEN(filepath), FS_IS_DIR, &rv);
+    if (Z_TYPE_P(&rv) == IS_FALSE) {
+        if (!php_stream_mkdir(Z_STRVAL(filepath), 0755, PHP_STREAM_MKDIR_RECURSIVE, NULL)) {
+            php_error_docref(NULL, E_ERROR, "EastWood Log directory creation failed %s", Z_STRVAL(filepath));
+            return;
+        }
+    }
 
-	php_stat(Z_STRVAL(filepath), Z_STRLEN(filepath), FS_IS_W, &rv);
-	if (Z_TYPE_P(&rv) == IS_FALSE) {
-		php_error_docref(NULL, E_ERROR, "EastWood Log directory does not have write access %s", Z_STRVAL(filepath));
-		return ;
-	}
+    php_stat(Z_STRVAL(filepath), Z_STRLEN(filepath), FS_IS_W, &rv);
+    if (Z_TYPE_P(&rv) == IS_FALSE) {
+        php_error_docref(NULL, E_ERROR, "EastWood Log directory does not have write access %s", Z_STRVAL(filepath));
+        return;
+    }
 
-	array_init(&list);
-	add_next_index_string(&list, LOGGER_FILENAME_PREFIX);
+    array_init(&list);
+    add_next_index_string(&list, "app");
 
-	ZVAL_STRING(&rotate, INI_STR("logger.rotate"));
+    ZVAL_STRING(&rotate, INI_STR("logger.rotate"));
+    for(i = 0; i <= logger_rotate_intval(Z_STRVAL(rotate)); i++) {
+        format[0] = date_format[i];
+        str = php_format_date(format, 1, time(NULL), 1);
+        add_next_index_string(&list, ZSTR_VAL(str));
+        zend_string_release(str);
+    }
 
-	for(i = 0; i <= logger_rotate_intval(Z_STRVAL(rotate)); i++) {
-		format[0] = date_format[i];
-		str = php_format_date(format, 1, time(NULL), 1);
-		add_next_index_string(&list, ZSTR_VAL(str));
-		zend_string_release(str);
-	}
+    str = zend_string_init("-", 1, 0);
+    php_implode(str, &list, &rv);
+    zend_string_free(str);
 
-	str = zend_string_init("-", 1, 0);
-	php_implode(str, &list, &rv);
-	
-	zval_ptr_dtor(&rv);
-	zend_string_release(str);
+    ZVAL_STRING(&filename, string_sprintf("%s/%s.log", Z_STRVAL(filepath), Z_STRVAL(rv)));
+    zval_ptr_dtor(&rv);
 
-	str = zend_string_init(Z_STRVAL(filepath), Z_STRLEN(filepath), 0);
-	filename = zend_string_concat_ex(str, 3, "/", Z_STRVAL(rv), LOGGER_FILENAME_AFTERFIX);
-	zend_string_release(str);
+    php_stat(Z_STRVAL(filename), Z_STRLEN(filename), FS_IS_FILE, &rv);
+    if (Z_TYPE_P(&rv) == IS_TRUE) {
+        php_stat(Z_STRVAL(filename), Z_STRLEN(filename), FS_IS_W, &rv);
+        if (Z_TYPE_P(&rv) == IS_FALSE) {
+            php_error_docref(NULL, E_ERROR, "EastWood Log file has no write permission %s", Z_STRVAL(filename));
+            return;
+        }
+    }
 
-	php_stat(ZSTR_VAL(filename), ZSTR_LEN(filename), FS_IS_FILE, &rv);
-	if (Z_TYPE_P(&rv) == IS_TRUE) {
-		//zval_ptr_dtor(&rv);
-		php_stat(ZSTR_VAL(filename), ZSTR_LEN(filename), FS_IS_W, &rv);
-		if (Z_TYPE_P(&rv) == IS_FALSE) {
-			php_error_docref(NULL, E_ERROR, "EastWood Log file has no write permission %s", ZSTR_VAL(filename));
-			return ;
-		}
-	}
+    ZVAL_STRING(&LOGGER_G(logging_filename), Z_STRVAL(filename));
 
-	//ZVAL_STR(&LOGGER_G(logging_filename), filename);
-	LOGGER_G(logging_filename) = ZSTR_VAL(filename);
-	php_printf("filename.first: %s\n", LOGGER_G(logging_filename));
-
-	zval_ptr_dtor(&rv);
-	zval_ptr_dtor(&list);
-	zval_ptr_dtor(&rotate);
-	zval_ptr_dtor(&filepath);
-	zend_string_release(filename);
-}
-
-static void logger_init()
-{
-	zval init;
-	array_init_size(&init, 0);
-	zend_update_static_property(logger_ce, ZEND_STRL(LOGGER_CLASS_PROPERTY_NAME), &init TSRMLS_CC);
-	zval_ptr_dtor(&init);
-
-	array_init_size(&LOGGER_G(logging_stack), 0);
-
-}
-
-static void logger_cleaner()
-{
-	php_printf("filename.last: %s\n", LOGGER_G(logging_filename));
-
-	zval *val;
-	//php_stream *stream = php_stream_open_wrapper(Z_STRVAL(LOGGER_G(logging_filename)), "ab", IGNORE_PATH|IGNORE_URL_WIN, NULL);
-	//if (stream == NULL) return;
-	//php_printf("filename: %s\n", Z_STRVAL_P(&LOGGER_G(logging_filename)));
-	ZEND_HASH_FOREACH_VAL(Z_ARRVAL_P(&LOGGER_G(logging_stack)), val) {
-		php_printf("yield: %s\n", Z_STRVAL_P(val));
-		//php_stream_write(stream, Z_STRVAL_P(val), Z_STRLEN_P(val));
-		//zval_ptr_dtor(val);
-	} ZEND_HASH_FOREACH_END();
-	//php_stream_close(stream);
-
-	//zval_ptr_dtor(&LOGGER_G(logging_filename));
-	zval_ptr_dtor(&LOGGER_G(logging_stack));
-}
-
-static void logger_format(zval *result, char *search, size_t search_len, char *replace, size_t replace_len)
-{
-	zend_string *rv = php_str_to_str(
-		Z_STRVAL_P(result),
-		Z_STRLEN_P(result),
-		search,
-		search_len,
-		replace,
-		replace_len
-	);
-	zval_ptr_dtor(result);
-	ZVAL_STR(result, rv);
-	zend_string_free(rv);
+    zval_ptr_dtor(&rv);
+    zval_ptr_dtor(&list);
+    zval_ptr_dtor(&rotate);
+    zval_ptr_dtor(&filepath);
+    zval_ptr_dtor(&filename);
 }
 
 static int logger_factory(INTERNAL_FUNCTION_PARAMETERS, int level)
 {
-	zval rv, level_list;
-	zend_string *tmp, *tag, *message, *output;
+    zval rv, *var, log_level;
+    zend_string *tag, *message, *output;
 
-	array_init(&level_list);
-	add_next_index_string(&level_list, LOGGER_LEVEL_STR_INFO);
-	add_next_index_string(&level_list, LOGGER_LEVEL_STR_WARNING);
-	add_next_index_string(&level_list, LOGGER_LEVEL_STR_ERROR);
-	add_next_index_string(&level_list, LOGGER_LEVEL_STR_DEBUG);
-	add_next_index_string(&level_list, LOGGER_LEVEL_STR_VERBOSE);
+    if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "SS", &tag, &message) == FAILURE) {
+        return FAILURE;
+    }
 
-	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "SS", &tag, &message) == FAILURE) {
-		return FAILURE;
-	}
+    logger_environment_init();
 
-	zval_ptr_dtor(&rv);
+    array_init(&log_level);
+    add_next_index_string(&log_level, "info");
+    add_next_index_string(&log_level, "warning");
+    add_next_index_string(&log_level, "error");
+    add_next_index_string(&log_level, "debug");
+    add_next_index_string(&log_level, "verbose");
 
-	logger_environment_init();
+    ZVAL_STRING(&rv, INI_STR("logger.format"));
+    php_printf("format: %s\n", Z_STRVAL(rv));
 
-	php_printf("filename.second: %s\n", LOGGER_G(logging_filename));
+    var = get_server_var("HTTP_HOST");
+    logger_format_line(&rv, "{http_host}", Z_STRVAL_P(var));
+    logger_format_line(&rv, "{request_host}", Z_STRVAL_P(var));
+    //zval_ptr_dtor(var);
 
-	if (Z_TYPE(PG(http_globals)[TRACK_VARS_SERVER]) == IS_ARRAY) {
-		php_printf("_SERVER is exists\n");
-	}
+    var = get_server_var("REQUEST_URI");
+    logger_format_line(&rv, "{uri}", Z_STRVAL_P(var));
+    logger_format_line(&rv, "{request_uri}", Z_STRVAL_P(var));
+    //zval_ptr_dtor(var);
 
+    var = get_server_var("REQUEST_METHOD");
+    logger_format_line(&rv, "{verb}", Z_STRVAL_P(var));
+    logger_format_line(&rv, "{request_method}", Z_STRVAL_P(var));
+    //zval_ptr_dtor(var);
 
+    var = get_server_var("REMOTE_ADDR");
+    logger_format_line(&rv, "{request_addr}", Z_STRVAL_P(var));
+    //zval_ptr_dtor(var);
 
-	ZVAL_STRING(&rv, INI_STR("logger.format"));
-	//php_printf("format: %s\n", Z_STRVAL(rv));
+    var = get_server_var("REMOTE_PORT");
+    logger_format_line(&rv, "{request_addr}", Z_STRVAL_P(var));
+    //zval_ptr_dtor(var);
 
+    var = get_server_var("SERVER_ADDR");
+    logger_format_line(&rv, "{server_addr}", Z_STRVAL_P(var));
+    //zval_ptr_dtor(var);
 
+    var = get_server_var("SERVER_PORT");
+    logger_format_line(&rv, "{server_port}", Z_STRVAL_P(var));
+    //zval_ptr_dtor(var);
 
-	string_trim(INI_STR("logger.application"), &tmp);
-	output = zend_strpprintf(0, "%s", ZSTR_LEN(tmp) == 0 ? "-" : ZSTR_VAL(tmp));
-	logger_format(&rv, "{app}", sizeof("{app}") - 1, ZSTR_VAL(output), ZSTR_LEN(output));
-	logger_format(&rv, "{application}", sizeof("{application}") - 1, ZSTR_VAL(output), ZSTR_LEN(output));
-	zend_string_release(tmp);
-	zend_string_release(output);
+    var = get_server_var("SERVER_PROTOCOL");
+    logger_format_line(&rv, "{server_protocol}", Z_STRVAL_P(var));
+    //zval_ptr_dtor(var);
 
-	output = php_format_date("r", 1, time(NULL), 1);
-	logger_format(&rv, "{date_rfc}", sizeof("{date_rfc}") - 1, ZSTR_VAL(output), ZSTR_LEN(output));
-	zend_string_release(output);
+    var = get_server_var("HTTPS");
+    logger_format_line(&rv, "{request_scheme}", strcmp(Z_STRVAL_P(var), "-") == 0 ? "https" : "http" );
+    //zval_ptr_dtor(var);
 
-	php_printf("filename.third: %s\n", LOGGER_G(logging_filename));
+    var = get_server_var("HTTP_X_FORWARDED_PROTO");
+    logger_format_line(&rv, "{request_scheme}", strcmp(Z_STRVAL_P(var), "-") == 0 ? "http" : Z_STRVAL_P(var) );
+    //zval_ptr_dtor(var);
 
-	output = get_host_name();
-	logger_format(&rv, "{hostname}", sizeof("{hostname}") - 1, ZSTR_VAL(output), ZSTR_LEN(output));
-	zend_string_release(output);
+    var = get_server_var("HTTP_USER_AGENT");
+    logger_format_line(&rv, "{user_agent}", Z_STRVAL_P(var));
+    logger_format_line(&rv, "{request_user_agent}", Z_STRVAL_P(var));
+    //zval_ptr_dtor(var);
 
-	output = zend_strpprintf(0, "%s", get_current_user());
-	logger_format(&rv, "{current_user}", sizeof("{current_user}") - 1, ZSTR_VAL(output), ZSTR_LEN(output));
-	zend_string_release(output);
+    output = string_trim(INI_STR("logger.application"));
+    logger_format_line(&rv, "{app}", ZSTR_LEN(output) == 0 ? "-" : ZSTR_VAL(output));
+    logger_format_line(&rv, "{application}", ZSTR_LEN(output) == 0 ? "-" : ZSTR_VAL(output));
+    zend_string_release(output);
 
-	output = zend_strpprintf(0, "%.3f", microtime());
-	logger_format(&rv, "{timestamp}", sizeof("{timestamp}") - 1, ZSTR_VAL(output), ZSTR_LEN(output));
-	zend_string_release(output);
+    output = get_post_body();
+    php_printf("post_body: %s\n", ZSTR_LEN(output) == 0 ? "-" : ZSTR_VAL(output));
+    logger_format_line(&rv, "{request_body}", ZSTR_LEN(output) == 0 ? "-" : ZSTR_VAL(output));
+    zend_string_release(output);
 
-	zval *val = zend_hash_index_find(Z_ARRVAL(level_list), level);
-	output = zend_strpprintf(0, "%s", Z_STRVAL_P(val));
-	logger_format(&rv, "{level}", sizeof("{level}") - 1, ZSTR_VAL(output), ZSTR_LEN(output));
-	zval_ptr_dtor(val);
-	zend_string_release(output);
+    output = _gethostname();
+    logger_format_line(&rv, "{hostname}", ZSTR_VAL(output));
+    zend_string_release(output);
 
-	output = zend_strpprintf(0, "%d", getgid());
-	logger_format(&rv, "{gid}", sizeof("{gid}") - 1, ZSTR_VAL(output), ZSTR_LEN(output));
-	zend_string_release(output);
+    output = zend_strpprintf(0, "%d", getgid());
+    logger_format_line(&rv, "{gid}", ZSTR_VAL(output));
+    zend_string_release(output);
 
-	output = zend_strpprintf(0, "%d", getpid());
-	logger_format(&rv, "{pid}", sizeof("{pid}") - 1, ZSTR_VAL(output), ZSTR_LEN(output));
-	zend_string_release(output);
+    output = zend_strpprintf(0, "%d", getpid());
+    logger_format_line(&rv, "{pid}", ZSTR_VAL(output));
+    zend_string_release(output);
 
-	logger_format(&rv, "{tag}", sizeof("{tag}") - 1, ZSTR_VAL(tag), ZSTR_LEN(tag));
-	logger_format(&rv, "{message}", sizeof("{message}") - 1, ZSTR_VAL(message), ZSTR_LEN(message));
+    output = php_format_date("r", 1, time(NULL), 1);
+    logger_format_line(&rv, "{date_rfc}", ZSTR_VAL(output));
+    zend_string_release(output);
 
-	
+    output = zend_strpprintf(0, "%.3f", timestamp());
+    logger_format_line(&rv, "{timestamp}", ZSTR_VAL(output));
+    zend_string_release(output);
 
+    output = zend_hash_index_find_ptr(Z_ARRVAL(log_level), level);
+    logger_format_line(&rv, "{level}", ZSTR_VAL(output));
+    zend_string_release(output);
 
-	add_next_index_string(&LOGGER_G(logging_stack), Z_STRVAL(rv));
-	zval_ptr_dtor(&rv);
-	zval_ptr_dtor(&level_list);
+    output = get_current_user();
+    logger_format_line(&rv, "{current_user}", ZSTR_VAL(output));
+    zend_string_release(output);
 
-	php_printf("filename.fouth: %s\n", LOGGER_G(logging_filename));
+    logger_format_line(&rv, "{tag}", ZSTR_VAL(tag));
+    logger_format_line(&rv, "{message}", ZSTR_VAL(message));
 
-	return SUCCESS;
+    add_next_index_string(&LOGGER_G(logging_stack), Z_STRVAL(rv));
+    zval_ptr_dtor(&rv);
+    zval_ptr_dtor(&log_level);
+
+    return SUCCESS;
 }
 
 
 PHP_FUNCTION(logger_version)
 {
-	RETURN_STRING(PHP_LOGGER_VERSION);
+    RETURN_STRING(PHP_LOGGER_VERSION);
 }
 
 PHP_METHOD(logger, info)
 {
-	RETURN_LONG(logger_factory(INTERNAL_FUNCTION_PARAM_PASSTHRU, LOGGER_LEVEL_INFO));
+    RETURN_LONG(logger_factory(INTERNAL_FUNCTION_PARAM_PASSTHRU, LOGGER_LEVEL_INFO));
 }
 
 PHP_METHOD(logger, warning)
 {
-	RETURN_LONG(logger_factory(INTERNAL_FUNCTION_PARAM_PASSTHRU, LOGGER_LEVEL_WARNING));
+    RETURN_LONG(logger_factory(INTERNAL_FUNCTION_PARAM_PASSTHRU, LOGGER_LEVEL_WARNING));
 }
 
 PHP_METHOD(logger, error)
 {
-	RETURN_LONG(logger_factory(INTERNAL_FUNCTION_PARAM_PASSTHRU, LOGGER_LEVEL_ERROR));
+    RETURN_LONG(logger_factory(INTERNAL_FUNCTION_PARAM_PASSTHRU, LOGGER_LEVEL_ERROR));
 }
 
 PHP_METHOD(logger, debug)
 {
-	RETURN_LONG(logger_factory(INTERNAL_FUNCTION_PARAM_PASSTHRU, LOGGER_LEVEL_DEBUG));
+    RETURN_LONG(logger_factory(INTERNAL_FUNCTION_PARAM_PASSTHRU, LOGGER_LEVEL_DEBUG));
 }
 
 PHP_METHOD(logger, verbose)
 {
-	RETURN_LONG(logger_factory(INTERNAL_FUNCTION_PARAM_PASSTHRU, LOGGER_LEVEL_VERBOSE));
+    RETURN_LONG(logger_factory(INTERNAL_FUNCTION_PARAM_PASSTHRU, LOGGER_LEVEL_VERBOSE));
 }
 
 PHP_MINIT_FUNCTION(logger)
 {
-	ZEND_INIT_MODULE_GLOBALS(logger, php_logger_init_globals, NULL);
-	REGISTER_INI_ENTRIES();
-	INIT_CLASS_ENTRY(ce, LOGGER_CLASS_NS_NAME, logger_methods);
-	logger_ce = zend_register_internal_class(&ce TSRMLS_CC);
-	zend_declare_property_null(logger_ce, ZEND_STRL(LOGGER_CLASS_PROPERTY_NAME), ZEND_ACC_STATIC|ZEND_ACC_PUBLIC TSRMLS_CC);
-	zend_register_class_alias(LOGGER_CLASS_SHORT_NAME, logger_ce);
-	return SUCCESS;
+    ZEND_INIT_MODULE_GLOBALS(logger, php_logger_init_globals, NULL);
+    REGISTER_INI_ENTRIES();
+    INIT_CLASS_ENTRY(ce, "EastWood\\Log\\Logger", logger_methods);
+    logger_ce = zend_register_internal_class(&ce TSRMLS_CC);
+    zend_declare_property_null(logger_ce, ZEND_STRL("settings"), ZEND_ACC_STATIC|ZEND_ACC_PUBLIC TSRMLS_CC);
+    zend_register_class_alias("Logger", logger_ce);
+    return SUCCESS;
 }
 
 PHP_MSHUTDOWN_FUNCTION(logger)
 {
-	UNREGISTER_INI_ENTRIES();
-	return SUCCESS;
+    UNREGISTER_INI_ENTRIES();
+    return SUCCESS;
 }
 
 PHP_RINIT_FUNCTION(logger)
 {
-#if defined(COMPILE_DL_LOGGER) && defined(ZTS)
-	ZEND_TSRMLS_CACHE_UPDATE();
-#endif
-	logger_init();
-	return SUCCESS;
+    #if defined(COMPILE_DL_LOGGER) && defined(ZTS)
+        ZEND_TSRMLS_CACHE_UPDATE();
+    #endif
+    logger_begin();
+    return SUCCESS;
 }
 
 PHP_RSHUTDOWN_FUNCTION(logger)
 {
-	logger_cleaner();
-	return SUCCESS;
+    logger_end();
+    return SUCCESS;
 }
 
 PHP_MINFO_FUNCTION(logger)
 {
-	php_info_print_table_start();
-	php_info_print_table_row(2, "logger support", "enabled");
-	php_info_print_table_row(2, "logger version", PHP_LOGGER_VERSION);
-	php_info_print_table_row(2, "logger author", "eastwood<boss@haowei.me>");
-	php_info_print_table_end();
-	DISPLAY_INI_ENTRIES();
+    php_info_print_table_start();
+    php_info_print_table_row(2, "logger support", "enabled");
+    php_info_print_table_row(2, "logger version", PHP_LOGGER_VERSION);
+    php_info_print_table_row(2, "logger author", "eastwood<boss@haowei.me>");
+    php_info_print_table_end();
+    DISPLAY_INI_ENTRIES();
 }
 
 zend_module_entry logger_module_entry = {
-	STANDARD_MODULE_HEADER,
-	"logger",
-	logger_functions,
-	PHP_MINIT(logger),
-	PHP_MSHUTDOWN(logger),
-	PHP_RINIT(logger),	
-	PHP_RSHUTDOWN(logger),
-	PHP_MINFO(logger),
-	PHP_LOGGER_VERSION,
-	STANDARD_MODULE_PROPERTIES
+    STANDARD_MODULE_HEADER,
+    "logger",
+    logger_functions,
+    PHP_MINIT(logger),
+    PHP_MSHUTDOWN(logger),
+    PHP_RINIT(logger),  
+    PHP_RSHUTDOWN(logger),
+    PHP_MINFO(logger),
+    PHP_LOGGER_VERSION,
+    STANDARD_MODULE_PROPERTIES
 };
 
 #ifdef COMPILE_DL_LOGGER
@@ -314,3 +340,4 @@ ZEND_TSRMLS_CACHE_DEFINE()
 #endif
 ZEND_GET_MODULE(logger)
 #endif
+
--- php_logger.h
+++ php_logger.h
@@ -22,12 +22,9 @@
 #include "config.h"
 #endif
 
-#include <pwd.h>
 #include "php.h"
-#include "php_ini.h"
-#include "SAPI.h"
+#include "main/SAPI.h"
 #include <ext/date/php_date.h>
-#include <ext/standard/info.h>
 #include <ext/standard/php_string.h>
 #include <ext/standard/php_filestat.h>
 
@@ -51,14 +48,7 @@ extern zend_module_entry logger_module_entry;
 #include "TSRM.h"
 #endif
 
-
-//#define LOGGER_G(v) ZEND_MODULE_GLOBALS_ACCESSOR(logger, v)
-
-#ifdef ZTS
-#define LOGGER_G(v) TSRMG(logger_globals_id, zend_logger_globals *, v)
-#else
-#define LOGGER_G(v) (logger_globals.v)
-#endif
+#define LOGGER_G(v) ZEND_MODULE_GLOBALS_ACCESSOR(logger, v)
 
 #if defined(ZTS) && defined(COMPILE_DL_LOGGER)
 ZEND_TSRMLS_CACHE_EXTERN()
@@ -66,36 +56,21 @@ ZEND_TSRMLS_CACHE_EXTERN()
 
 #endif
 
-
-#define LOGGER_CLASS_NS_NAME            "EastWood\\Log\\Logger"
-#define LOGGER_CLASS_SHORT_NAME         "Logger"
-#define LOGGER_CLASS_PROPERTY_NAME      "settings"
-
-#define LOGGER_FILENAME_PREFIX          "app"
-#define LOGGER_FILENAME_AFTERFIX        ".log"
-
-#define LOGGER_LEVEL_STR_INFO           "info"
-#define LOGGER_LEVEL_STR_WARNING        "warning"
-#define LOGGER_LEVEL_STR_ERROR          "error"
-#define LOGGER_LEVEL_STR_DEBUG          "debug"
-#define LOGGER_LEVEL_STR_VERBOSE        "verbose"
-
 #define LOGGER_LEVEL_INFO               0
 #define LOGGER_LEVEL_WARNING            1
 #define LOGGER_LEVEL_ERROR              2
 #define LOGGER_LEVEL_DEBUG              3
 #define LOGGER_LEVEL_VERBOSE            4
 
-
 #define LOGGER_ROTATE_YEAR              0
 #define LOGGER_ROTATE_MONTH             1
-#define LOGGER_ROTATE_DAILY			    2
+#define LOGGER_ROTATE_DAILY             2
 
 static zend_class_entry *logger_ce, ce;
 
 ZEND_BEGIN_MODULE_GLOBALS(logger)
-  zval logging_stack;
-  zval logging_filename;
+    zval logging_stack;
+    zval logging_filename;
 ZEND_END_MODULE_GLOBALS(logger)
 
 ZEND_DECLARE_MODULE_GLOBALS(logger)
@@ -109,19 +84,19 @@ PHP_METHOD(logger, debug);
 PHP_METHOD(logger, verbose);
 
 PHP_INI_BEGIN()
-  PHP_INI_ENTRY("logger.path", "/var/log", PHP_INI_ALL, NULL)
-  PHP_INI_ENTRY("logger.rotate", "", PHP_INI_ALL, NULL)
-  PHP_INI_ENTRY("logger.format", "{date_rfc}||{timestamp}||{level}||{tag}||{message}", PHP_INI_ALL, NULL)
-  PHP_INI_ENTRY("logger.application", "", PHP_INI_ALL, NULL)
+    PHP_INI_ENTRY("logger.path", "/var/log", PHP_INI_ALL, NULL)
+    PHP_INI_ENTRY("logger.rotate", "", PHP_INI_ALL, NULL)
+    PHP_INI_ENTRY("logger.format", "{date_rfc}||{timestamp}||{level}||{tag}||{message}", PHP_INI_ALL, NULL)
+    PHP_INI_ENTRY("logger.application", "", PHP_INI_ALL, NULL)
 PHP_INI_END()
 
 const zend_function_entry logger_methods[] = {
-  PHP_ME(logger, info, NULL, ZEND_ACC_PUBLIC|ZEND_ACC_STATIC)
-  PHP_ME(logger, warning, NULL, ZEND_ACC_PUBLIC|ZEND_ACC_STATIC)
-  PHP_ME(logger, error, NULL, ZEND_ACC_PUBLIC|ZEND_ACC_STATIC)
-  PHP_ME(logger, debug, NULL, ZEND_ACC_PUBLIC|ZEND_ACC_STATIC)
-  PHP_ME(logger, verbose, NULL, ZEND_ACC_PUBLIC|ZEND_ACC_STATIC)
-  PHP_FE_END
+    PHP_ME(logger, info, NULL, ZEND_ACC_PUBLIC|ZEND_ACC_STATIC)
+    PHP_ME(logger, warning, NULL, ZEND_ACC_PUBLIC|ZEND_ACC_STATIC)
+    PHP_ME(logger, error, NULL, ZEND_ACC_PUBLIC|ZEND_ACC_STATIC)
+    PHP_ME(logger, debug, NULL, ZEND_ACC_PUBLIC|ZEND_ACC_STATIC)
+    PHP_ME(logger, verbose, NULL, ZEND_ACC_PUBLIC|ZEND_ACC_STATIC)
+    PHP_FE_END
 };
 
 const zend_function_entry logger_functions[] = {
@@ -141,107 +116,99 @@ static int logger_rotate_intval(const char *str)
     return -1;
 }
 
-static zend_string *zend_string_concat(zend_string *str1, const char *str2)
+char* string_replace(char *source, char *search, char *replace)
 {
-    zend_string *rv;
-    rv = zend_strpprintf(0, "%s%s", ZSTR_VAL(str1), str2);
-    zend_string_release(str1);
-    return rv;
+    int pos;
+    int offset;
+    int source_len = strlen(source);
+    int search_len = strlen(search);
+    int replace_len = strlen(replace);
+    char *buf = NULL;
+    char *needle;
+
+    while((needle = strstr(source, search))) {
+        offset = 0;
+        //printf("in buf: %s\n", source);
+        pos = needle - source;
+        //printf("needle: %s pos: %d\n", needle, pos);
+        buf = (char*)malloc(source_len + replace_len);
+        memcpy(buf, source, pos);
+        //printf("cpy buf: %s\n", buf);
+        offset += pos;
+        memcpy(buf + offset, replace, replace_len);
+        //printf("rpy buf: %s\n", buf);
+        offset += replace_len;
+        memcpy(buf + offset, source + pos + search_len, source_len - pos - search_len);
+        offset += source_len - pos - search_len;
+        source = strdup(buf);
+        source_len = strlen(source);
+        buf[offset] = '\0';
+        //printf("output buf: %s\n", buf);
+    }
+    if (buf == NULL) buf = strdup(source);
+    
+    return buf;
 }
 
-static zend_string *zend_string_concat_ex(zend_string *str, int count, ...)
+
+static char* string_sprintf(const char *format, ...)
 {
-    va_list ap;
-    va_start(ap, count);
-    while(count--) str = zend_string_concat(str, va_arg(ap, char*));
-    va_end(ap);
-    return str;
+    char *s;
+    va_list args;
+    va_start(args, format);
+    vsprintf(s, format, args);
+    va_end(args);
+    return s;
 }
 
-static zval* call_user_func(const char *func_name, int num, zval param[])
+static zend_string* string_trim(const char *subject)
 {
-    zval rv, function, *rv_ptr;
-    ZVAL_STRING(&function, func_name);
-    call_user_function(EG(function_table), NULL, &function, &rv, num, param);
-    rv_ptr = &rv;
-    return rv_ptr;
+    zend_string *tmp = zend_string_init(subject, strlen(subject), 0);
+    zend_string *result = php_trim(tmp, NULL, 0, 3);
+    zend_string_free(tmp);
+    return result;
 }
 
-static double microtime()
+static double timestamp()
 {
     struct timeval tv;
     gettimeofday(&tv, NULL);
     return tv.tv_sec + (double)tv.tv_usec / 1000000;
 }
 
-static char *get_current_user()
+static zend_string *get_current_user()
 {
     struct passwd *pwd;
     pwd = getpwuid(getuid());
-    return pwd->pw_name;
+    return zend_string_init(pwd->pw_name, strlen(pwd->pw_name), 0);
 }
 
-static void string_trim(const char *subject, zend_string **result)
-{
-    zend_string *tmp = zend_strpprintf(0, "%s", subject);
-    *result = php_trim(tmp, NULL, 0, 3);
-    zend_string_release(tmp);
-}
-
-static zend_string* get_host_name()
+static zend_string *_gethostname()
 {
     char buf[255];
-    zend_string *result;
     gethostname(buf, sizeof(buf));
-    string_trim(buf, &result);
-    return result;
+    return zend_string_init(buf, strlen(buf), 0);
 }
 
-
-static void get_strval(const char *key, zval *val)
+static zval* get_server_var(const char* key)
 {
-    //zend_string *zkey;
-    //zkey = zend_strpprintf(0, "_SERVER");
-    //val = zend_hash_find(&EG(symbol_table), zkey);
-    //zend_string_release(zkey);
+    zval val, *ht, *value;
+    ZVAL_STRING(&val, "-");
+    ht = zend_hash_str_find(&EG(symbol_table), ZEND_STRL("_SERVER"));
+    value = zend_hash_str_find(Z_ARRVAL_P(ht), key, strlen(key));
+    if (value == NULL) value = &val;
+    if (Z_TYPE_P(value) != IS_STRING) convert_to_string(value);
+    zval_ptr_dtor(&val);
+    return value;
+}
 
-    /*zend_long num_key;
-    zend_string *str_key;
-    zval *v;
-    ZEND_HASH_FOREACH_KEY_VAL(zend_rebuild_symbol_table(), num_key, str_key, v) {
-        if (Z_TYPE_P(v) == IS_STRING) {
-            php_printf("(string) %s=%s\n", ZSTR_VAL(str_key), Z_STRVAL_P(v));
-        } else if (Z_TYPE_P(v) == IS_DOUBLE) {
-            php_printf("(double) %s=%d\n", ZSTR_VAL(str_key), Z_DVAL_P(v));
-        } else if (Z_TYPE_P(v) == IS_ARRAY) {
-            php_printf("(array) %s=[array]\n", ZSTR_VAL(str_key));
-        }
-    } ZEND_HASH_FOREACH_END();*/
-
-    /*if (zend_is_auto_global_str("_SERVER", sizeof("_SERVER") - 1) == SUCCESS) {
-        php_printf("_SERVER is exists\n");
-    }else{
-        php_printf("_SERVER not exists\n");
-    }*/
-
-    php_printf("uri: %s\n", SG(request_info).request_uri);
-    php_printf("verb: %s\n", SG(request_info).request_method);
-
-
-    //if (val == NULL) php_printf("_SERVER: nil\n");
-
-
-    //zkey = zend_strpprintf(0, "%s", key);
-    //php_printf("b: %d\n", zend_hash_exists(ht, zkey));
-
-    /*zval *v;
-    ZEND_HASH_FOREACH_VAL(ht, v) {
-        if (Z_TYPE_P(v) == IS_STRING) {
-            php_printf("strval: %s\n", Z_STRVAL_P(v));
-        }else if(Z_TYPE_P(v) == IS_DOUBLE) {
-            php_printf("dval: %d\n", Z_DVAL_P(v));
-        }
-    } ZEND_HASH_FOREACH_END();*/
+static zend_string* get_post_body()
+{
+    zend_long maxlen = (ssize_t) PHP_STREAM_COPY_ALL;
+    php_stream *stream = php_stream_open_wrapper("php://input", "rb", REPORT_ERRORS, NULL);
+    zend_string *body = php_stream_copy_to_mem(stream, maxlen, 0);
+    php_stream_close(stream);
+    return body == NULL ? ZSTR_EMPTY_ALLOC() : body;
 }
 
 /*
